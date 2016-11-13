58f1ec6cf1e38fc59b444bb680a5864acd1d7d69
gauryn <gauryn22@gmail.com>
2016-11-13 10:03:17 -0500

    Alternate login screen that is responsive to every screen size and orientation

--- Reimburse Final Poject/ViewController.swift
+++ Reimburse Final Poject/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tartanFooter: UIImageView!
+    @IBOutlet weak var appBody: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewDidLayoutSubviews(){
+        super.viewDidLayoutSubviews()
+        // set rounded corners of footer
+        setRoundedCornersOfBody()
+    }
+    
+    func setRoundedCornersOfBody(){
+        let rectShape = CAShapeLayer()
+        rectShape.bounds = self.appBody.bounds
+        rectShape.path = UIBezierPath(roundedRect: self.appBody.bounds, byRoundingCorners: [.bottomLeft , .bottomRight], cornerRadii: CGSize(width: 20, height: 20)).cgPath
+        rectShape.position = self.appBody.center
+        self.appBody.layer.mask = rectShape
+        
+    }
 
 }
 
