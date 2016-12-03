eaf77faa37e47c10ddebbdb72e51e2da23d29aa6
Rumby Wilson <erwilson@andrew.cmu.edu>
2016-12-02 22:00:52 -0500

    Inital file upload of image and zip feature - needs one bug in apple cache complier fixed

---
+++ Documents/Junior/iOS/ReceiptScan/FakeObjectiveCClass.h
@@ -0,0 +1,13 @@
+//
+//  FakeObjectiveCClass.h
+//  ReceiptScan
+//
+//  Created by Gaury Nagaraju on 10/30/16.
+//  Copyright © 2016 Gaury Nagaraju. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FakeObjectiveCClass : NSObject
+
+@end
